e567df06cbb9755a39111350e4ee4a5f95e0017d
albertobonfiglio <darthbert>
2024-05-16 20:07:00 +1000

    development (APP) * started angular app setup

---
+++ frontend/src/app/core/core.module.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { APP_INITIALIZER, NgModule, Optional, SkipSelf, isDevMode } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterModule } from "@angular/router";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { env } from "process";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+   
+  ],
+  providers: [
+   /* {
+      provide: APP_INITIALIZER, // delays the start of the application by 3 secs
+      useValue: () =>
+        new Promise((resolve) => setTimeout(resolve, env.splashDelay)),
+        //new Promise((resolve) => setTimeout(resolve, 1)),
+      multi: true,
+    },
+    */
+  ],
+  exports: [],
+})
+
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error('CoreModule is already loaded. Import only in AppModule');
+    }
+  }
+}
---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  apiUri: 'http://localhost:5002',
+  Auth: {
+    clientId: 'iQAVMWxA8pqv0LRikpzGbLg04WIKHg6u',
+    clientSecret: '1qJoRZvJftg3zH691V4rJTY1WIDXqt6w7idq6I8nGGvLgg4O-oNgUi32UMNiBbQ_',
+    domain: 'darthbert-udacity.au.auth0.com',
+  }
+};
